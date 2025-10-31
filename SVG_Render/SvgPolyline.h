e9a26f00fb46a96a5526271cb429204f2ac11389
Tiến Đạt Đào <tiendat1376@gmail.com>
2025-10-31 21:57:55 +0700

    nntanh

--- SVG_Render/SvgPolyline.h
+++ SVG_Render/SvgPolyline.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "SvgElement.h"
+#include <vector>
+
+using namespace tinyxml2;
+using namespace std;
+
+class SvgPolyline : public SvgElement {
+private:
+	vector<Vector2> points;
+	bool isClosed;
+public:
+	SvgPolyline();
+	~SvgPolyline();
+};
