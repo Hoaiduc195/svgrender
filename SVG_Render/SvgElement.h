b9421a2bfd11243b1afae7cba3fe5a03224b1834
KhoaTapCode2006 <khoaanhcoder@gmail.com>
2025-10-31 15:04:45 +0000

    Create and edit SvgElement.h

---
+++ SVG_Render/SvgElement.h
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <gdiplus.h>
+
+using namespace std;
+using namespace Gdiplus;
+
+class SvgElement {
+    private:
+        string id;
+        float opacity;
+        Color fill;
+        Color stroke;
+    public:
+    void 
+        SvgElement() {
+            cout << "SvgElement created." << endl;
+        }
+        
+        void render() {
+            cout << "Rendering SVG Element." << endl;
+        }
+
+
+};
