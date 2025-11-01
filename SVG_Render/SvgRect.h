66cae03f5042fc8c2bf5567586c02d1779eaaa48
Anh Khoa <khoaanhcoder@gmail.com>
2025-11-01 22:09:17 +0700

    Changes .h files of classes: Circle, Ellipse, Line, Rect

--- SVG_Render/SvgRect.h
+++ SVG_Render/SvgRect.h
@@ -15,14 +15,7 @@ class SvgRect : public SvgElement {
         float rx;
         float ry;
     public:
-        SvgRect(float x, float y, float width, float height, float rx, float ry) : x(x), y(y), width(width), height(height), rx(rx), ry(ry) {
-            cout << "SvgRect created." << endl;
-        }
+        SvgRect(float x, float y, float width, float height, float rx, float ry);
 
-        void draw(Gdiplus::Graphics& graphics) override {
-            Pen pen(Gdiplus::Color(255, 0, 0, 0)); 
-            SolidBrush brush(Gdiplus::Color(255, 255, 0, 0));
-            graphics.FillRectangle(&brush, x, y, width, height);
-            graphics.DrawRectangle(&pen, x, y, width, height);
-        }
+        void draw(Gdiplus::Graphics& graphics) override;
 };
