b46dd80a6e146ddc609874490ebf76809b1e84f5
Hoaiduc195 <hoaiducduong19@gmail.com>
2025-11-09 09:01:48 +0700

    Fix SvgDocument.h

--- SVG_Render/SvgDocument.h
+++ SVG_Render/SvgDocument.h
@@ -2,11 +2,11 @@
 #include "SvgElement.h"
 #include "framework.h"
 
-class SvgDocument : public SvgElement {
+class SvgDocument{
 private:
     vector<unique_ptr<SvgElement>> elements;
 public:
     SvgDocument() = default;
     void addElement(unique_ptr<SvgElement>);
-    void draw(Graphics& graphics) override;
+    void draw(Graphics& graphics);
 };
